296a017c861ca93c7d7e027bd47874648d1d1935
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-03-02 18:46:29 +0900

    21.03.02_TabsView

---
+++ TabsView/TabsView/Demo2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Demo2ViewController.swift
+//  TabsView
+//
+//  Created by Derrick on 2021/03/02.
+//
+
+import UIKit
+
+class Demo2ViewController: UIViewController {
+    
+    //-----------
+    // Properties
+    //------------
+    
+    var pageIndex: Int!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TabsView/TabsView/TabsPageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabsPageViewController.swift
+//  TabsView
+//
+//  Created by Derrick on 2021/03/02.
+//
+
+import UIKit
+
+class TabsPageViewController: UIPageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
