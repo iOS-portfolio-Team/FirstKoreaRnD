5c1edf3cec22bec639c2b718e13a39a76e388298
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-03-02 18:49:25 +0900

    21.03.02_TabsView

---
+++ TabsView/TabsView/ViewController.swift
@@ -0,0 +1,182 @@
+//
+//  ViewController.swift
+//  TabsView
+//
+//  Created by Derrick on 2021/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //-----------
+    // Properties
+    //------------
+    
+
+    @IBOutlet weak var tabsView: TabsView!
+    var pageController: UIPageViewController!
+    var currentIndex: Int = 0
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabs()
+        setupPageViewController()
+
+    }
+
+    func setupTabs() {
+        // Add Tabs (Set 'icon'to nil if you don't want to have icons)
+        tabsView.tabs = [
+            Tab(icon: UIImage(systemName: "music.note"), title: "Music"),
+            Tab(icon: UIImage(systemName: "film.fill"), title: "Movies"),
+            Tab(icon: UIImage(systemName: "book.fill"), title: "Books")
+        ]
+        
+        // Set TabMode to '.fixed' for stretched tabs in full width of screen or '.scrollable' for scrolling to see all tabs
+        tabsView.tabMode = .fixed
+        
+        // TabView Customization
+        tabsView.titleColor = .white
+        tabsView.iconColor = .white
+        tabsView.indicatorColor = .white
+        tabsView.titleFont = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        tabsView.collectionView.backgroundColor = .cyan
+        
+        // Set TabsView Delegate
+        tabsView.delegate = self
+        
+        // Set the selected Tab when the app starts
+        tabsView.collectionView.selectItem(at: IndexPath(item: 0, section: 0), animated: true, scrollPosition: .centeredVertically)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Refresh CollectionView Layout when you rotate the device
+        tabsView.collectionView.collectionViewLayout.invalidateLayout()
+    }
+    func setupPageViewController() {
+           // PageViewController
+           self.pageController = storyboard?.instantiateViewController(withIdentifier: "TabsPageViewController") as! TabsPageViewController
+           self.addChild(self.pageController)
+           self.view.addSubview(self.pageController.view)
+           
+           // Set PageViewController Delegate & DataSource
+           pageController.delegate = self
+           pageController.dataSource = self
+           
+           // Set the selected ViewController in the PageViewController when the app starts
+           pageController.setViewControllers([showViewController(0)!], direction: .forward, animated: true, completion: nil)
+           
+           // PageViewController Constraints
+           self.pageController.view.translatesAutoresizingMaskIntoConstraints = false
+           NSLayoutConstraint.activate([
+               self.pageController.view.topAnchor.constraint(equalTo: self.tabsView.bottomAnchor),
+               self.pageController.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+               self.pageController.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+               self.pageController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+           ])
+           self.pageController.didMove(toParent: self)
+       }
+    // Show ViewController for the current position
+    func showViewController(_ index: Int) -> UIViewController? {
+        if (self.tabsView.tabs.count == 0) || (index >= self.tabsView.tabs.count) {
+            return nil
+        }
+        
+        currentIndex = index
+        
+        if index == 0 {
+            let contentVC = storyboard?.instantiateViewController(withIdentifier: "Demo1ViewController") as! Demo1ViewController
+            contentVC.pageIndex = index
+            return contentVC
+        } else if index == 1 {
+            let contentVC = storyboard?.instantiateViewController(withIdentifier: "Demo2ViewController") as! Demo2ViewController
+            contentVC.pageIndex = index
+            return contentVC
+        } else if index == 2 {
+            let contentVC = storyboard?.instantiateViewController(withIdentifier: "Demo3ViewController") as! Demo3ViewController
+            contentVC.pageIndex = index
+            return contentVC
+        } else {
+            let contentVC = storyboard?.instantiateViewController(withIdentifier: "Demo1ViewController") as! Demo1ViewController
+            contentVC.pageIndex = index
+            return contentVC
+        }
+    }
+}//---END
+extension ViewController: TabsDelegate {
+    func tabsViewDidSelectItemAt(position: Int) {
+        // Check if the selected tab cell position is the same with the current position in pageController, if not, then go forward or backward
+        if position != currentIndex {
+            if position > currentIndex {
+                self.pageController.setViewControllers([showViewController(position)!], direction: .forward, animated: true, completion: nil)
+            } else {
+                self.pageController.setViewControllers([showViewController(position)!], direction: .reverse, animated: true, completion: nil)
+            }
+            tabsView.collectionView.scrollToItem(at: IndexPath(item: position, section: 0), at: .centeredHorizontally, animated: true)
+        }
+    }
+}
+extension ViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    // return ViewController when go forward
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let vc = pageViewController.viewControllers?.first
+        var index: Int
+        index = getVCPageIndex(vc)
+        // Don't do anything when viewpager reach the number of tabs
+        if index == tabsView.tabs.count {
+            return nil
+        } else {
+            index += 1
+            return self.showViewController(index)
+        }
+    }
+    
+    // return ViewController when go backward
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let vc = pageViewController.viewControllers?.first
+        var index: Int
+        index = getVCPageIndex(vc)
+        
+        if index == 0 {
+            return nil
+        } else {
+            index -= 1
+            return self.showViewController(index)
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if finished {
+            if completed {
+                guard let vc = pageViewController.viewControllers?.first else { return }
+                let index: Int
+                
+                index = getVCPageIndex(vc)
+                
+                tabsView.collectionView.selectItem(at: IndexPath(item: index, section: 0), animated: true, scrollPosition: .centeredVertically)
+                // Animate the tab in the TabsView to be centered when you are scrolling using .scrollable
+                tabsView.collectionView.scrollToItem(at: IndexPath(item: index, section: 0), at: .centeredHorizontally, animated: true)
+            }
+        }
+    }
+    
+    // Return the current position that is saved in the UIViewControllers we have in the UIPageViewController
+    func getVCPageIndex(_ viewController: UIViewController?) -> Int {
+        switch viewController {
+        case is Demo1ViewController:
+            let vc = viewController as! Demo1ViewController
+            return vc.pageIndex
+        case is Demo2ViewController:
+            let vc = viewController as! Demo2ViewController
+            return vc.pageIndex
+        case is Demo3ViewController:
+            let vc = viewController as! Demo3ViewController
+            return vc.pageIndex
+        default:
+            let vc = viewController as! Demo1ViewController
+            return vc.pageIndex
+        }
+    }
+}
