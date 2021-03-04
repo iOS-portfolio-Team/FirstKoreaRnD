bc389dfb99e4034f4708920434ec9b1b69391dda
jonghancha <jonghancha@gmail.com>
2021-03-04 19:14:59 +0900

    03-04 종한 : ,,

---
+++ myPage/myPage/myPage/ChangePwViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ChangePwViewController.swift
+//  myPage
+//
+//  Created by TJ on 2021/03/04.
+//
+
+import UIKit
+
+class ChangePwViewController: UIViewController {
+
+    @IBOutlet weak var tfPw: UITextField!
+    @IBOutlet weak var tfPw2: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ myPage/myPage/myPage/Share.swift
@@ -0,0 +1,12 @@
+//
+//  Share.swift
+//  myPage
+//
+//  Created by TJ on 2021/03/04.
+//
+
+import Foundation
+
+struct Share {
+    static var userID: String = "cescfab@naver.com"
+}
